edcbf0dc0c791706f3eaa1d86f5e2d476e8e0d3d
liuhaoyu-git <a350121811@163.com>
2019-10-12 10:24:38 +0800

    新建登录分支新建登录组件配置路由

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <template>
   <div id="app">
     <!-- <img src="./assets/logo.png"> -->
-    <h1>123</h1>
     <router-view/>
   </div>
 </template>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,18 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+//@ 指的是src文件夹
 // import HelloWorld from '@/components/HelloWorld'
+import Login from '@/components/login/login.vue'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      // name: 'HelloWorld',
-      // component: HelloWorld
+      nmae:'login',
+      path: '/login',
+      component:Login
+
     }
   ]
 })
